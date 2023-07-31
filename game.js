75e0310d4536ea9cc7d9b6eb3daeb61a2bee25c5
gusteugenio <137667932+gusteugenio@users.noreply.github.com>
2023-07-30 23:34:05 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,15 @@
 function getComputerChoice() {
-    const choice = Math.floor(Math.random() * 3);
-    const compch = 0
-    if (choice == 1){
+    const choice = Math.floor(Math.random() * 3) + 1;
+    let compch = ''
+    if (choice === 1){
         compch = 'Rock'
         console.log(compch)
-    } else if(choice == 2){
+    } else if(choice === 2){
         compch = 'Paper'
         console.log(compch)
-    } else if(choice == 3){
+    } else if(choice === 3){
         compch = 'Scissors'
         console.log(compch)
-    } else {
-        console.log('Error')
     }
 }
+
